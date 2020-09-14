c53a050a34588a3b8c8f47da4f107de34937f04f
joorgeroberto <jorgercj@dcomp.ufs.br>
2020-09-13 22:49:40 -0300

    (fix): Corrigindo a lógica para comissao de bonés

--- Vendedor.playground/Contents.swift
+++ Vendedor.playground/Contents.swift
@@ -11,10 +11,10 @@ import UIKit
 //- Encapsule todos os métodos necessários.
 
 class Vendedor {
-    private var nome: String;
-    private var idade: Int;
-    private var cpf: String;
-    private var saldoEmConta: Float;
+    private var nome: String
+    private var idade: Int
+    private var cpf: String
+    private var saldoEmConta: Float
     
     init(nome: String, idade: Int, cpf: String, saldoEmConta: Float) {
         self.nome = nome
@@ -23,64 +23,65 @@ class Vendedor {
         self.saldoEmConta = saldoEmConta
     }
     public func getNome() -> String  {
-        return(self.nome);
+        return(self.nome)
     }
     
     public func setNome(_ nome: String) {
-        self.nome = nome;
+        self.nome = nome
     }
     
     public func getIdade() -> Int  {
-        return(self.idade);
+        return(self.idade)
     }
     
     public func setIdade(_ idade: Int) {
-        self.idade = idade;
+        self.idade = idade
     }
     
     public func getCpf() -> String  {
-        return(self.cpf);
+        return(self.cpf)
     }
     
     public func setCpf(_ cpf: String) {
-        self.cpf = cpf;
+        self.cpf = cpf
     }
     
     public func getSaldoEmConta() -> Float  {
-        return(self.saldoEmConta);
+        return(self.saldoEmConta)
     }
     
     private func setSaldoEmConta(_ valor: Float) {
-        self.saldoEmConta += valor;
+        self.saldoEmConta += valor
     }
     
     public func vender(quantidadeDePecas: Int, tipoDePeca: String) {
         var valorDaVenda: Float = 0;
         if(tipoDePeca == "terno") {
             if(quantidadeDePecas < 3) {
-                valorDaVenda = Float(quantidadeDePecas) * 400;
+                valorDaVenda = Float(quantidadeDePecas) * 400
                 print("\(valorDaVenda)")
             }
             else {
-                valorDaVenda = Float(quantidadeDePecas * 400) - Float(50 * quantidadeDePecas);
+                valorDaVenda = Float(quantidadeDePecas * 400) - Float(50 * quantidadeDePecas)
                 print("\(valorDaVenda)")
             }
         }
         else if(tipoDePeca == "vestido") {
             if(quantidadeDePecas < 5) {
-                valorDaVenda = Float(quantidadeDePecas) * 900;
+                valorDaVenda = Float(quantidadeDePecas) * 900
                 print("\(valorDaVenda)")
             }
             else {
-                valorDaVenda = Float(quantidadeDePecas * 900) - 250;
+                valorDaVenda = Float(quantidadeDePecas * 900) - 250
                 print("\(valorDaVenda)")
             }
         }
         else if(tipoDePeca == "bone") {
             valorDaVenda = Float(quantidadeDePecas * 50);
-            let quantidadeDeBrindes = quantidadeDePecas / 3;
+            let quantidadeDeBrindes = quantidadeDePecas / 2;
+            
             if(quantidadeDeBrindes > 0) {
-                valorDaVenda -= Float(valorDaVenda - Float(50 * quantidadeDeBrindes) );
+                valorDaVenda -= Float(50 * quantidadeDeBrindes)
                 print("\(valorDaVenda)")
             }
         }
@@ -89,10 +90,35 @@ class Vendedor {
     }
 }
 
-let vendedor = Vendedor(nome: "Gustavo", idade: 36, cpf: "12312312312", saldoEmConta: 0)
-vendedor.vender(quantidadeDePecas: 10, tipoDePeca: "terno")
-print(vendedor.getSaldoEmConta())
+let joao = Vendedor(nome: "Joao", idade: 36, cpf: "12312312312", saldoEmConta: 0)
+joao.vender(quantidadeDePecas: 3, tipoDePeca: "terno")
+print(joao.getSaldoEmConta())
+joao.vender(quantidadeDePecas: 1, tipoDePeca: "terno")
+print(joao.getSaldoEmConta())
 
 
-vendedor.vender(quantidadeDePecas: 1, tipoDePeca: "vestido")
-print(vendedor.getSaldoEmConta())
+print("\n")
+
+let marcelo = Vendedor(nome: "Marcelo", idade: 26, cpf: "31231231212", saldoEmConta: 0)
+marcelo.vender(quantidadeDePecas: 5, tipoDePeca: "vestido")
+print(marcelo.getSaldoEmConta())
+marcelo.vender(quantidadeDePecas: 1, tipoDePeca: "vestido")
+print(marcelo.getSaldoEmConta())
+
+print("\n")
+
+let marcos = Vendedor(nome: "Marcos", idade: 20, cpf: "23123123121", saldoEmConta: 0)
+marcos.vender(quantidadeDePecas: 3, tipoDePeca: "bone")
+print(marcos.getSaldoEmConta())
+
+print("\n")
+
+let jorge = Vendedor(nome: "Jorge", idade: 25, cpf: "23123123121", saldoEmConta: 0)
+jorge.vender(quantidadeDePecas: 2, tipoDePeca: "bone")
+print(jorge.getSaldoEmConta())
+
+print("\n")
+
+let ulisses = Vendedor(nome: "Ulisses", idade: 25, cpf: "23123123121", saldoEmConta: 0)
+ulisses.vender(quantidadeDePecas: 6, tipoDePeca: "bone")
+print(ulisses.getSaldoEmConta())
